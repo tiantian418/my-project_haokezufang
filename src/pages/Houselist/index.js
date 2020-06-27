fa0509eeb7670116c4168007d848cf13c1e892e7
xtt5221 <1395408068@qq.com>
2020-06-27 14:11:24 +0800

    完成找房页面的列表功能

--- src/pages/Houselist/index.js
+++ src/pages/Houselist/index.js
@@ -5,12 +5,13 @@ import Filter from './components/Filter'
 import SearchHeader from '../../components/SearchHeder'
 
 import './houselist.scss'
+import styles from './houselist.module.scss'
 
 import { getCurrentCity } from '../../utils/index'
 
 import { API } from '../../utils/api'
 
-import { List } from 'react-virtualized'
+import { List, AutoSizer, WindowScroller, InfiniteLoader } from 'react-virtualized'
 
 class Houselist extends React.Component {
   state= {
@@ -24,6 +25,10 @@ class Houselist extends React.Component {
     this.setState({
       cityname: dingwei.label
     })
+    // 进入页面 获取房子列表
+    // this.filters应该没有条件 默认获取所有
+    this.filters = {} // 没有条件
+    this.searchHouseList()
   }
 
   onFilter = (filters) => {
@@ -54,17 +59,81 @@ class Houselist extends React.Component {
 
   // 每条数据 渲染的函数  里面可以写 html内容
   rowRenderer=({
-    key, // Unique key within array of rows
-    index, // Index of row within collection
-    isScrolling, // The List is currently being scrolled
-    isVisible, // This row is visible within the List (eg it is not an overscanned row)
-    style, // Style object to be applied to row (to position it)
+    key, // 唯一key
+    index, // 索引
+    // isScrolling, // 正在滚动
+    // isVisible, // 是否可见
+    style, // 每个大盒子div 必须写 控制样式
   })=> {
+    // 渲染每一个房子
+    let house = this.state.list[index] // 拿出每一条数据
+    // console.log('房子数据', house)
+
+    if (!house) {
+      // 没有房子 渲染一个loading
+      return <div key={key} style={style} className={styles.loading}>
+        正在加载中...
+      </div>
+    }
+    // 有房子才渲染
     return (
-      <div key={key} style={style}>
-          <h2>哈哈哈哈哈</h2>
+      <div key={key} style={style} className={styles.house}>
+        {/* 左边图片 */}
+        <div className={styles.imgWrap}>
+          <img className={styles.img} src={`http://api-haoke-web.itheima.net${house.houseImg}`} alt="" />
+        </div>
+        {/* 右边文字 */}
+        <div className={styles.content}>
+          <h3 className={styles.title}>{house.title}</h3>
+          <div className={styles.desc}>{house.desc}</div>
+          <div>
+            {/* ['近地铁', '随时看房'] */}
+            {
+              house.tags.map((item, index) => {
+                return <span key={index} className={[styles.tag,styles.tag1 ].join(' ')} >
+                  {item}
+                </span>
+              })
+            }
+          </div>
+          <div className={styles.price}>
+            <span className={styles.priceNum}>{house.price}</span> 元/月
+          </div>
+        </div>
       </div>
     )
+  }
+
+  // 当前数据是否加载完成
+  isRowLoaded = ({ index }) => {
+    // 返回true或者false
+    return !!this.state.list[index]
+  }
+
+  // 加载更多
+  loadMoreRows = ({ startIndex, stopIndex }) => {
+    console.log('加载更多')
+    return new Promise( async resolve => {
+      // 发送请求 获取更多数据
+      let dingwei = await getCurrentCity()
+      let res = await API.get('/houses', {
+        params: { // 参数
+          cityId: dingwei.value,  // 城市id
+          ...this.filters, // 条件
+          start: startIndex, // 从1开始
+          end: stopIndex // 取到20 结束
+        }
+      })
+      // 追加合并数组
+      let newList = [...this.state.list, ...res.data.body.list]
+      this.setState({
+        count: res.data.body.count, // 总条数
+        list: newList // 房子
+      })
+
+      // 获取数据成功后 resolve
+      resolve()
+    })
   }
 
 
@@ -82,13 +151,47 @@ class Houselist extends React.Component {
       <Filter onFilter={this.onFilter}></Filter>
 
       {/* 房子列表 */}
-      <List
-        width={300}
-        height={300}
+      {/* 帮我们计算剩余屏幕宽高 */}
+      {/* <AutoSizer>
+        {({height, width}) => (
+          <List
+            width={width}
+            height={height}
+            rowCount={this.state.count}
+            rowHeight={120}
+            rowRenderer={this.rowRenderer}
+          />
+        )}
+      </AutoSizer> */}
+      <InfiniteLoader
+        isRowLoaded={this.isRowLoaded}
+        loadMoreRows={this.loadMoreRows}
         rowCount={this.state.count}
-        rowHeight={120}
-        rowRenderer={this.rowRenderer}
-      />
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <WindowScroller>
+            {({ height, isScrolling,onChildScroll, scrollTop }) => (
+              <AutoSizer>
+                {({ width }) => (
+                  <List
+                    autoHeight // 设置高度为 WindowScroller 最终渲染的列表高度
+                    isScrolling={isScrolling}
+                    onScroll={onChildScroll}
+                    scrollTop={scrollTop} // 让list滚多少
+                    onRowsRendered={onRowsRendered}
+                    ref={registerChild}
+                    width={width} // 视口宽度
+                    height={height} // 视口高度
+                    rowCount={this.state.count} // List列表项总条目数
+                    rowHeight={120} // 每一行高度
+                    rowRenderer={this.rowRenderer} //每条数据的渲染函数 
+                  />
+                )}
+              </AutoSizer>
+            )}
+          </WindowScroller>
+        )}
+      </InfiniteLoader>
     </div>
   }
 }
